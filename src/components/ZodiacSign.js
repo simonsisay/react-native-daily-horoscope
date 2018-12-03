4b2ef3f2ccdf08065e0a9c7e0b79e84dd1ba7eda
Simon Sisay <Simonsisay9@gmail.com>
2018-12-03 21:58:49 +0300

    flatlisted

--- src/components/ZodiacSign.js
+++ src/components/ZodiacSign.js
@@ -1,20 +1,36 @@
 import React from 'react'
-import { View, Text, TouchableHighlight } from 'react-native'
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 const ZodiacSign = ({iconName, zodiac, navigation}) => {
 	return(
 		<TouchableHighlight 
-			style={{padding:20}}
+			style={styles.sign}
 			onPress={() => navigation.navigate('read', {zodiacSign:zodiac.toLowerCase()})}>
-			<View style={{marginBottom:15}}>
+			<View style={{marginBottom:15, justifyContent:'center', alignItems:'center'}}>
 				<MaterialCommunityIcons name={`${iconName}`} size={100} color="#a86e12" />
-				<Text style={{textAlign:'center', color:'white'}}>
+				<Text style={{color:'white', paddingBottom:10}}>
 					{zodiac}
 				</Text>
 			</View>
 		</TouchableHighlight>
 	)
 }
+
+const styles = StyleSheet.create({
+	sign:{
+		width:120,
+		height:120,
+		backgroundColor:'rgba(77, 9, 132, .5)', 
+		borderRadius:30, 
+		overflow:'hidden',
+		shadowColor: '#606060',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.6,
+      shadowRadius: 5,
+      elevation: 1,
+
+	}
+})
 
 export default ZodiacSign
--- src/screens/HoroscopesListScreen.js
+++ src/screens/HoroscopesListScreen.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableHighlight, ScrollView } from 'react-native'
+import { View, Text, TouchableHighlight, ScrollView, FlatList } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import ZodiacSign from '../components/ZodiacSign'
-
+import { horoscopeList } from '../data/horoscopeList'
 
 class HoroscopesListScreen extends Component{
 	render(){
 		return(
 			<View style={{flex:1, backgroundColor:'#3a2766'}}>
 				<ScrollView>
-					<View style={{flex:1, flexDirection:'row', justifyContent:'space-between', flexWrap:'wrap'}}>
-						<ZodiacSign iconName="zodiac-cancer" zodiac="CANCER" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-aquarius" zodiac="AQUARIUS" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-aries" zodiac="ARIES" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-capricorn" zodiac="CAPRICORN" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-leo" zodiac="LEO" navigation={this.props.navigation} />
-						<ZodiacSign iconName="zodiac-libra" zodiac="LIBRA" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-pisces" zodiac="PISCES" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-sagittarius" zodiac="SAGITTARIUS" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-scorpio" zodiac="SCORPIO" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-taurus" zodiac="TAURUS" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-gemini" zodiac="GEMINI" navigation={this.props.navigation}/>
-						<ZodiacSign iconName="zodiac-virgo" zodiac="VIRGO" navigation={this.props.navigation}/>
+					<View 
+						style={{
+							flex:1,
+						}}
+					 >
+						<FlatList 
+							data={horoscopeList}
+							numColumns={2}
+							columnWrapperStyle={{
+								flexDirection:'row',
+								flexWrap:'wrap',
+								flex:1,
+								justifyContent:'space-between',
+								alignItems:'center',
+								padding:20
+
+							}}
+							renderItem={({item}) => (
+								<ZodiacSign iconName={item.iconName} zodiac={item.zodiac} navigation={this.props.navigation} />
+							)}
+							keyExtractor={item => item.zodiac}
+						/>
 					</View>
 				</ScrollView>
 			</View>
 		)
 	}
 }
-
 
 export default HoroscopesListScreen
